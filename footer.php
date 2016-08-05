dc8143af22a6c0ec45290073fb64e42fedcf4702
Luke Twyman <neverest_songs@yahoo.co.uk>
2016-08-05 16:40:06 +0100

    Updated email cta

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
 				<a class="social" href="http://www.facebook.com/blokdust">facebook</a> <div class="spaceLine col1"></div> <a class="social" href="http://www.twitter.com/blokdust">twitter</a> <div class="spaceLine col1"></div> <a class="social" href="https://www.youtube.com/channel/UCukBbnIMiUZBbD4fJHrcHZQ">youtube</a>
 
 				<div id="email" class="col8">
-					Send suggestions or hello's<br/>
-					to:
+					Get in touch with us at:<br/>
 					<a href="mailto:blokdust@gmail.com">blokdust@gmail.com</a>
 				</div>
 			</div>
